52d7a270aa73dcc34a06bb888025de0749c70b29
bhanuteja6 <bhanuteja6@gmail.com>
2012-10-11 22:54:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaminari::Application.config.secret_token = 'bf214b791f8f6901ffdce2bbe2d0790e66276eff4998b472545babda2a18111f838863c9c48cdf40f54ed9ebdce926b1ef797023b556453ca60ac2c259b941f7'
